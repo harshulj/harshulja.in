ab2f8ea06936a2a1608e0ef2b85e4cda0806b6c4
Harshul Jain <harshulj@gmail.com>
2015-05-23 13:53:12 +0530

    Not removing optional tags.

--- Gruntfile.js
+++ Gruntfile.js
@@ -239,7 +239,7 @@ module.exports = function (grunt) {
           removeAttributeQuotes: true,
           removeCommentsFromCDATA: true,
           removeEmptyAttributes: true,
-          removeOptionalTags: true,
+          removeOptionalTags: false,
           removeRedundantAttributes: true,
           useShortDoctype: true
         },
